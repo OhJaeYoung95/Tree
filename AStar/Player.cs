e927517dbad66964a15b3e4ea7e1e5f990c2aa9d
OhJaeYoung <ostyoung@naver.com>
2023-02-09 21:30:13 +0900

    AStar Part1

---
+++ AStar/Player.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace AStar
+{
+	class Pos
+	{
+		public Pos(int y, int x) { Y = y; X = x; }
+		public int Y;
+		public int X;
+	}
+
+	class Player
+	{
+		public int PosY { get; private set; }
+		public int PosX { get; private set; }
+		Random _random = new Random();
+		Board _board;
+
+		enum Dir
+		{
+			Up = 0,
+			Left = 1,
+			Down = 2,
+			Right = 3
+		}
+
+		int _dir = (int)Dir.Up;
+		List<Pos> _points = new List<Pos>();
+
+		public void Initialize(int posY, int posX, Board board)
+		{
+			PosY = posY;
+			PosX = posX;
+			_board = board;
+
+			//BFS();
+			AStar();
+		}
+
+		struct PQNode : IComparable<PQNode>
+        {
+			public int F;
+			public int G;
+			public int Y;
+			public int X;
+
+            public int CompareTo(PQNode other)
+            {
+				if (F == other.F)
+					return 0;
+				return F < other.F ? 1 : -1;
+            }
+        }
+
+		void AStar()
+        {
+			// 점수 매기기
+			// F = G + H
+			// F = 최종 점수 (작을수록 좋음, 경로에 따라 달라짐)
+			// G = 시작점에서 해당 좌표까지  이동하는데 드는 비용 (작을수록 좋음, 경로에 따라 달라짐)
+			// H = 목적지에서 얼마나 가까운지 (작을수록 좋음, 고정)
+
+			// (y,x) 이미 방문했는지 여부 (방문 = closed 상태)
+			bool[,] closed = new bool[_board.Size, _board.Size];        // CloseList
+
+			// (y,x) 가는 길을 한 번이라도 발견했는지
+			// 발견X => MaxValue
+			// 발견O => F = G + H
+			int[,] open = new int[_board.Size, _board.Size];        // OpenList
+			for (int y = 0; y < _board.Size; y++)
+				for (int x = 0; x < _board.Size; x++)
+					open[y, x] = Int32.MaxValue;
+
+			// 오픈리스트에 있는 정보들 중에서, 가장 좋은 후보를 빠르게 뽑아오기 위한 도구
+			PriorityQueue<PQNode> pq = new PriorityQueue<PQNode>();
+
+			// 시작점 발견 (예약 진행)
+			open[PosY, PosX] = Math.Abs(_board.DestY - PosY) + Math.Abs(_board.DestX - PosX);
+			pq.Push(new PQNode() { F = Math.Abs(_board.DestY - PosY) + Math.Abs(_board.DestX - PosX), G = 0, Y = PosY, X = PosX});
+
+            while (true)
+            {
+				// 제일 좋은 후보를 찾는다
+
+            }
+        }
+
+		void BFS()
+		{
+			int[] deltaY = new int[] { -1, 0, 1, 0 };
+			int[] deltaX = new int[] { 0, -1, 0, 1 };
+
+			bool[,] found = new bool[_board.Size, _board.Size];
+			Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+			Queue<Pos> q = new Queue<Pos>();
+			q.Enqueue(new Pos(PosY, PosX));
+			found[PosY, PosX] = true;
+			parent[PosY, PosX] = new Pos(PosY, PosX);
+
+			while (q.Count > 0)
+			{
+				Pos pos = q.Dequeue();
+				int nowY = pos.Y;
+				int nowX = pos.X;
+
+				for (int i = 0; i < 4; i++)
+				{
+					int nextY = nowY + deltaY[i];
+					int nextX = nowX + deltaX[i];
+
+					if (nextX < 0 || nextX >= _board.Size || nextY < 0 || nextY >= _board.Size)
+						continue;
+					if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+						continue;
+					if (found[nextY, nextX])
+						continue;
+
+					q.Enqueue(new Pos(nextY, nextX));
+					found[nextY, nextX] = true;
+					parent[nextY, nextX] = new Pos(nowY, nowX);
+				}
+			}
+
+			int y = _board.DestY;
+			int x = _board.DestX;
+			while (parent[y, x].Y != y || parent[y, x].X != x)
+			{
+				_points.Add(new Pos(y, x));
+				Pos pos = parent[y, x];
+				y = pos.Y;
+				x = pos.X;
+			}
+			_points.Add(new Pos(y, x));
+			_points.Reverse();
+		}
+
+		void RightHand()
+		{
+			// 현재 바라보고 있는 방향을 기준으로, 좌표 변화를 나타낸다
+			int[] frontY = new int[] { -1, 0, 1, 0 };
+			int[] frontX = new int[] { 0, -1, 0, 1 };
+			int[] rightY = new int[] { 0, -1, 0, 1 };
+			int[] rightX = new int[] { 1, 0, -1, 0 };
+
+			_points.Add(new Pos(PosY, PosX));
+			// 목적지 도착하기 전에는 계속 실행
+			while (PosY != _board.DestY || PosX != _board.DestX)
+			{
+				// 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는지 확인.
+				if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
+				{
+					// 오른쪽 방향으로 90도 회전
+					_dir = (_dir - 1 + 4) % 4;
+					// 앞으로 한 보 전진.
+					PosY = PosY + frontY[_dir];
+					PosX = PosX + frontX[_dir];
+					_points.Add(new Pos(PosY, PosX));
+				}
+				// 2. 현재 바라보는 방향을 기준으로 전진할 수 있는지 확인.
+				else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty)
+				{
+					// 앞으로 한 보 전진.
+					PosY = PosY + frontY[_dir];
+					PosX = PosX + frontX[_dir];
+					_points.Add(new Pos(PosY, PosX));
+				}
+				else
+				{
+					// 왼쪽 방향으로 90도 회전
+					_dir = (_dir + 1 + 4) % 4;
+				}
+			}
+		}
+
+		const int MOVE_TICK = 100;
+		int _sumTick = 0;
+		int _lastIndex = 0;
+		public void Update(int deltaTick)
+		{
+			if (_lastIndex >= _points.Count)
+				return;
+
+			_sumTick += deltaTick;
+			if (_sumTick >= MOVE_TICK)
+			{
+				_sumTick = 0;
+
+				PosY = _points[_lastIndex].Y;
+				PosX = _points[_lastIndex].X;
+				_lastIndex++;
+			}
+		}
+	}
+}
